ebc541639293683cafbc155bc4f11d165d29e529
Sleepergamedev <gustav.sanden996@gmail.com>
2024-09-18 14:49:14 +0200

    la till flera nya funktioner
    rita, pausa, ui element

--- Sandén_Gustav_Game_Of_Life/Assets/Scripts/GridManagement.cs
+++ Sandén_Gustav_Game_Of_Life/Assets/Scripts/GridManagement.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using JetBrains.Annotations;
+using TMPro;
 using UnityEditor.U2D.Animation;
 using UnityEngine;
 
@@ -6,20 +8,20 @@ public class GridManagement : MonoBehaviour
 {
 
     public GameObject go; //sprite som printas
-    //[HideInInspector] public int nearbyLivingCells; //hur många levande celler som finns nära
-
-
     [Range(0, 2)]
     public float tickRate;
     [Range(0, 5000)]
     public float cameraSize;
     private float tickValue = 0;
+    public TextMeshProUGUI pauseText;
     [Range(0, 1)]
     public float cellAliveProbability;
     [SerializeField] public int maxX = 10; //x kolumn
     [SerializeField] public int maxY = 10; //y kolumn
-    private Camera mainCamera;
-
+    private Camera mainCamera; //kamera
+    private bool pause = true; //bool för att pausa
+    private Vector3 mousePos; //mus position
+    private Vector3 worldPos; //musen position i världen
     Cell[,] Grid; //gör en jagged array som tar in typen Cell
 
     void Start()
@@ -28,27 +30,28 @@ public class GridManagement : MonoBehaviour
         Grid = new Cell[maxX, maxY];
         Application.targetFrameRate = 60;
         {
+
+
             for (int i = 0; i < maxX; i++)
             {
                 for (int j = 0; j < maxY; j++)
                 {
                     Grid[i, j] = new Cell(i, j, Instantiate(go).GetComponent<SpriteRenderer>(), maxX, maxY);
-                    if (Random.value >= cellAliveProbability)
-                    {
-                        Grid[i, j].disableCell();
-                    }
-
+                    Grid[i, j].sprite.enabled = false;
                 }
             }
+
+
         }
     }
     void Update()
     {
         mainCamera.orthographicSize = cameraSize;
-
+        mousePos = Input.mousePosition;
+        worldPos = mainCamera.ScreenToWorldPoint(mousePos);
         tickValue += Time.deltaTime;
 
-        if (tickRate <= tickValue)
+        if (tickRate <= tickValue && pause == false)
         {
             for (int i = 0; i < maxX; i++)
             {
@@ -66,8 +69,48 @@ public class GridManagement : MonoBehaviour
             }
             tickValue = 0;
         }
+        if (Input.GetMouseButton(0) && pause == true)
+        {
+            Vector2Int gridPos = WorldPosToGridPos(worldPos);
+            if (gridPos.x < 0 || gridPos.y < 0 || gridPos.x >= maxX || gridPos.y >= maxY)
+            {
+                Debug.Log("Out of bounds");
+            }
+            else
+                Grid[gridPos.x, gridPos.y].enableCell();
+        }
 
     }
+    public Vector2Int WorldPosToGridPos(Vector3 mousePos)
+    {
+        mousePos.x -= -maxX / 2;
+        mousePos.y -= -maxY / 2;
+        return new Vector2Int(Mathf.RoundToInt(mousePos.x), Mathf.RoundToInt(mousePos.y));
+    }
+    public void PauseGame()
+    {
+        if (pause == false)
+        {
+            pause = true;
+            pauseText.text = "Unpause";
+        }
+        else
+        {
+            pause = false;
+            pauseText.text = "Pause";
+        }
+    }
+    public void ClearAllCells()
+    {
+        for (int i = 0; i < maxX; i++)
+        {
+            for (int j = 0; j < maxY; j++)
+            {
+                Grid[i, j].disableCell();
+            }
+        }
+    }
+
     int CheckAliveCells(int x, int y) //funktion som kollar hur många levande celler som finns i närheten
     {
         int nearbyLivingCells = 0;
@@ -99,16 +142,17 @@ public class GridManagement : MonoBehaviour
             }
         }
         return nearbyLivingCells;
-
     }
 
 
 
     public class Cell //cell klass
     {
-        public bool isAlive = true; //kollar ifall cellen lever eller är död
+        public bool isAlive = false; //kollar ifall cellen lever eller är död
+
         public int howManyNeighbour = 0;
         public SpriteRenderer sprite;
+
         public void applyRules()
         {
             if (howManyNeighbour < 2 && isAlive)
@@ -134,7 +178,7 @@ public class GridManagement : MonoBehaviour
             sprite.enabled = true;
             isAlive = true;
         }
-        public Cell(float x, float y, SpriteRenderer sprite, int maxX, int maxY) //konstruktor som tar in x och y koordinat samt ett gameobjekt
+        public Cell(int x, int y, SpriteRenderer sprite, int maxX, int maxY) //konstruktor som tar in x och y koordinat samt ett gameobjekt
         {
             this.sprite = sprite;
             isAlive = true; //sätt att cellen är död som standard
---
+++ Sandén_Gustav_Game_Of_Life/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void pause()
+    {
+        Debug.Log("test");
+    }
+
+    public void clearCells()
+    {
+
+    }
+
+
+}
